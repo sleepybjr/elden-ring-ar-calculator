d26ef85c7476490d4fbc7016b9998145ee992d78
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2022-04-09 19:24:16 -0700

    update todo

--- src/App.js
+++ src/App.js
@@ -3,9 +3,8 @@ import FilterableWeaponTable from './FilterableWeaponTable';
 
 // TODO: add better styling
 // add input validation
-// add debuff for insufficient required stats, add buff if eligible after two-handing
 // make multiselect boxes better and intuitive (such as adding select all and deselect all and using checkboxes?)
-// sort weapon names by actual weapon name and not full weapon name
+// sort weapon names by actual weapon name and not full weapon name?
 // add ability to remove / add columns to table, scroll horizontal wheel for large column table?
 // add passives
 function App() {
