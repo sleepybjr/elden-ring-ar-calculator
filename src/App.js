39f30a836ef9e8ff9882e059fe4238a89e6e4520
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2022-04-10 19:34:32 -0700

    remove comment

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FilterableWeaponTable from './FilterableWeaponTable';
 // TODO: add better styling
 // make multiselect boxes better and intuitive (such as adding select all and deselect all and using checkboxes?)
 // add passives
-// fix bows
 
 function App() {
     return (
