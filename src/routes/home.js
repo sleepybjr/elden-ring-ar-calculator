7b6384b630806b8ee12574a3b4a90d7a4803a016
sleepybjr <22383876+sleepybjr@users.noreply.github.com>
2022-04-29 00:49:39 -0700

    add info onto home page

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,28 @@
 import UserBuild from "../userBuild/UserBuild";// eslint-disable-line no-unused-vars
 
+import { Link } from "react-router-dom";
+
 export default function Home() {
 
     return (
         <div className='extra-spacing'>
-            WIP.
+            <br />
+            <br />
+            Check out the header above to get to the weapon AR calculator.
+            <br />
+            <br />
+            There is also a new armor optimizer calculator.
+            <br />
+            <br />
+            <Link to="/elden-ring-ar-calculator">Click here</Link> to get to the weapon AR calculator.
+            <br />
+            <br />
+            <Link to="/armor-optimizer">Click here</Link> to get to the armor optimizer.
+            <br />
+            <br />
+            <br/>
+            <br/>
+
             {/* <UserBuild/> */}
         </div>
     );
